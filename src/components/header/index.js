9a569970298949b426e8bde93b6dcc442ec5113c
sujan <dev.mern.sujan@gmail.com>
2022-09-17 10:47:14 +0600

    tailwind setup & header part done

---
+++ src/components/header/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { AiOutlineMail } from "react-icons/ai";
+import { FiPhoneCall } from "react-icons/fi";
+import { FaFacebookF, FaLinkedinIn } from "react-icons/fa";
+import { BsInstagram } from "react-icons/bs";
+import { GrTwitter } from "react-icons/gr";
+
+const Header = () => {
+  return (
+    <header className="bg-black py-4	">
+      <div className="max-w-container mx-auto">
+        <div className="flex">
+          <div className="w-2/4 flex">
+            <p
+              className="font-pop text-xs font-normal relative pl-6 text-white after:absolute after:top-[-3px] after:right-[-28px] after:w-0.5
+            after:h-full after:content-[''] after:bg-yellowborder"
+            >
+              <AiOutlineMail className="absolute left-0 top-0	text-lg" />
+              mail@yourcompany.com
+            </p>
+            <p className="font-pop text-xs font-normal relative pl-6 text-white ml-14	">
+              <FiPhoneCall className="absolute left-0 top-0	text-lg" />
+              +896 120 5889 (Toll free)
+            </p>
+          </div>
+          <div className="w-2/4 text-white flex justify-end gap-x-5">
+            <FaFacebookF />
+            <GrTwitter />
+            <FaLinkedinIn />
+            <BsInstagram />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
