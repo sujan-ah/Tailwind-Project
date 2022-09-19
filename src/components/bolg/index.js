b22cd111dac61543b181295108ace7819f87ca2e
sujan <dev.mern.sujan@gmail.com>
2022-09-19 13:11:34 +0600

    blog part some code change

--- src/components/bolg/index.js
+++ src/components/bolg/index.js
@@ -6,56 +6,62 @@ const Blog = () => {
     <section>
       <div
         className="h-124 pt-[136px] pb-[136px]"
-        style={{ background: "url(images/backgroundcompany.png)" }}
+        style={{ background: "url(images/backgroundcompany.png) no-repeat" }}
       >
         <div className="max-w-container mx-auto">
           <div className="flex gap-x-14">
             <div
-              className="w-2/6 bg-primary "
-              style={{ background: "url(images/blogimg3.png)" }}
+              className="w-2/6 bg-primary group"
+              style={{ background: "url(images/blogimg3.png) no-repeat" }}
             >
-              <h4 className="font-pop font-bold text-[24px] text-white w-[320px] px-[40px] pt-[75px]">
-                lorem ipsum dolor sit amet consectetur adipiscing elit sed do
-                eiusmod.
-              </h4>
-              <a
-                href="#"
-                className="inline-block bg-[#888583] font-pop text-[12px] font-semibold text-white border py-2.5 px-5 my-[50px] ml-[46px] hover:bg-transparent ease-linear	duration-300"
-              >
-                Read more
-              </a>
+              <div className="w-full h-full bg-darklight ">
+                <h4 className="font-pop font-bold text-[24px] text-white w-[320px] px-[40px] pt-[75px] group-hover:text-primary ">
+                  lorem ipsum dolor sit amet consectetur adipiscing elit sed do
+                  eiusmod.
+                </h4>
+                <a
+                  href="#"
+                  className="inline-block bg-[#888583] font-pop text-[12px] font-semibold text-white border py-2.5 px-5 my-[50px] ml-[46px] hover:bg-transparent ease-linear	duration-300"
+                >
+                  Read more
+                </a>
+              </div>
             </div>
 
             <div
-              className="w-2/6 bg-primary"
-              style={{ background: "url(images/blogimg2.png)" }}
+              className="w-2/6 bg-primary group"
+              style={{ background: "url(images/blogimg2.png) no-repeat" }}
             >
-              <h4 className="font-pop font-bold text-[24px] text-white w-[320px] px-[44px] pt-[75px]	">
-                lorem ipsum dolor sit amet consectetur adipiscing elit sed do
-                eiusmod.
-              </h4>
-              <a
-                href="#"
-                className="inline-block bg-[#888583] font-pop text-[12px] font-semibold text-white border py-2.5 px-5 my-[50px] ml-[46px] hover:bg-transparent ease-linear	duration-300"
-              >
-                Read more
-              </a>
+              <div className="w-full h-full bg-darklight">
+                <h4 className="font-pop font-bold text-[24px] text-white w-[320px] px-[44px] pt-[75px]	group-hover:text-primary">
+                  lorem ipsum dolor sit amet consectetur adipiscing elit sed do
+                  eiusmod.
+                </h4>
+                <a
+                  href="#"
+                  className="inline-block bg-[#888583] font-pop text-[12px] font-semibold text-white border py-2.5 px-5 my-[50px] ml-[46px] hover:bg-transparent ease-linear	duration-300"
+                >
+                  Read more
+                </a>
+              </div>
             </div>
 
             <div
-              className="w-2/6 bg-primary"
+              className="w-2/6 bg-primary group"
               style={{ background: "url(images/blogimg1.png)" }}
             >
-              <h4 className="font-pop font-bold text-[24px] text-white w-[320px] px-[44px] pt-[75px]	">
-                lorem ipsum dolor sit amet consectetur adipiscing elit sed do
-                eiusmod.
-              </h4>
-              <a
-                href="#"
-                className="inline-block bg-[#888583] font-pop text-[12px] font-semibold text-white border py-2.5 px-5 my-[50px] ml-[46px] hover:bg-transparent ease-linear	duration-300"
-              >
-                Read more
-              </a>
+              <div className="w-full h-full bg-darklight">
+                <h4 className="font-pop font-bold text-[24px] text-white w-[320px] px-[44px] pt-[75px]	group-hover:text-primary">
+                  lorem ipsum dolor sit amet consectetur adipiscing elit sed do
+                  eiusmod.
+                </h4>
+                <a
+                  href="#"
+                  className="inline-block bg-[#888583] font-pop text-[12px] font-semibold text-white border py-2.5 px-5 my-[50px] ml-[46px] hover:bg-transparent ease-linear	duration-300"
+                >
+                  Read more
+                </a>
+              </div>
             </div>
           </div>
 
